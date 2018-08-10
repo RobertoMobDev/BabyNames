9d2fd4dffc1756015e794485de278363df276b8a
rcervantes <rcervantes@ia.com.mx>
2018-08-10 13:04:19 -0500

    Se elimina Caso de Uso Abstracto para recuperar información del perfil de Facebook

--- login/src/main/java/com/babynames/login/domain/useCases/RecoverFacebookInfoUseCase.kt
+++ login/src/main/java/com/babynames/login/domain/useCases/RecoverFacebookInfoUseCase.kt
@@ -6,33 +6,31 @@ import com.babynames.core.domain.useCases.UseCase
 import com.babynames.login.domain.entities.requestObjects.SignInRequestObject
 import com.babynames.login.domain.exceptions.SignInException
 import com.facebook.GraphRequest
-import com.facebook.GraphResponse
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class RecoverFacebookInfoUseCase @Inject constructor(threadExecutor: ThreadExecutor, postExecutionThread: PostExecutionThread) : AbstractRecoverFacebookInfoUseCase(threadExecutor, postExecutionThread) {
-
+class RecoverFacebookInfoUseCase @Inject constructor(threadExecutor: ThreadExecutor, postExecutionThread: PostExecutionThread) : UseCase<SignInRequestObject, GraphRequest>(threadExecutor, postExecutionThread) {
+    override fun createObservable(params: GraphRequest): Observable<SignInRequestObject> {
+        return Observable.defer {
+            Observable.just(params.executeAndWait()).map {
+                if (it.error != null) {
+                    throw SignInException(SignInException.Type.FACEBOOK_GRAPH_ERROR, it.error.errorMessage)
+                } else {
+                    val json = it.jsonObject
+                    val userAge = json.getJSONObject("age_range")?.getString("min") ?: "0"
+                    val pic = json.getJSONObject("picture")
+                    val picUrl = pic.getJSONObject("data")?.getString("url") ?: ""
+                    val signInRequestObject = SignInRequestObject("CreateUser",
+                            json.getString("id"),
+                            json.getString("email") ?: "",
+                            picUrl,
+                            if (json.has("gender")) json.getString("gender") else "",
+                            userAge,
+                            json.getString("name") ?: "")
+                    signInRequestObject
+                }
+            }
+        }
+    }
 }
-
-
-//                    .map {
-//                if (it.error != null) {
-//                    throw SignInException(SignInException.Type.FACEBOOK_GRAPH_ERROR, it.error.errorMessage)
-//                } else {
-//                    val json = it.jsonObject
-//                    val age = json.getJSONObject("user_age")
-//                    val userAge = age.getString("min")
-//                    val pic = json.getJSONObject("picture")
-//                    val picData = pic.getJSONObject("data")
-//                    val picUrl = picData.getString("url")
-//                    val signInRequestObject = SignInRequestObject("",
-//                            json.getString("id"),
-//                            json.getString("email"),
-//                            picUrl,
-//                            json.getString("gender"),
-//                            userAge,
-//                            json.getString("name"))
-//                    signInRequestObject
-//                }
-//            }
 
